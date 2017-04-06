aa92fb403fd9a6ab5a6011eb6f38361d6ff059d6
Daniel Delago <ddelago0596@gmail.com>
2017-04-06 13:32:51 -0500

    Update trace.sh

--- trace.sh
+++ trace.sh
@@ -3,7 +3,7 @@ lttng create demo_session -o ./out
 lttng enable-event -u -a
 lttng add-context -u -t vpid -t vtid -t procname -t ip
 lttng start
-LD_PRELOAD=liblttng-ust-cyg-profile.so:liblttng-ust-dl.so deployer -s deployment.xml
+LD_PRELOAD=liblttng-ust-cyg-profile.so:liblttng-ust-dl.so deployer -s start.ops
 lttng stop
 lttng destroy
 babeltrace ./out/ > trace_data.txt
